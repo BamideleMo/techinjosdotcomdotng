ba92f66cafa3424923a247f848f98ff3d79f26b0
bamidelemo <bamidelemo@gmail.com>
2024-08-12 05:13:28 +0100

    .

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,7 +14,7 @@ function About() {
       />
       <div>
         <Header />
-        <div class="pt-20 md:pt-28">
+        <div class="pt-24 md:pt-28">
           <div class="w-full md:w-11/12 mx-auto backgound-color md:p-12 lg:p-12">
             <div class="content md:w-10/12 lg:w-6/12 mx-auto space-y-3">
               <div class="bg-white p-2 md:p-6">
