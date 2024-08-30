12266ba82ee3c9b632b4f3bd6cbb1c5007638408
bamidelemo <bamidelemo@gmail.com>
2024-08-30 18:18:12 +0100

    .

--- src/pages/Issue.jsx
+++ src/pages/Issue.jsx
@@ -99,7 +99,7 @@ function Issue() {
                   <>
                     <h2 class="text-center text-2xl md:text-5xl uppercase mb-4 font-bold">
                       <span class="block">Tech Newsletter</span>
-                      <span class="block">
+                      <span class="block -mt-1">
                         for & from Jos<b class="text-red-600">.</b>
                       </span>
                     </h2>
