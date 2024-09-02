106dca1bc02d860822bf66e5daf8973d857f82e3
bamidelemo <bamidelemo@gmail.com>
2024-09-03 00:32:13 +0100

    .

--- src/pages/Issue.jsx
+++ src/pages/Issue.jsx
@@ -87,7 +87,7 @@ function Issue() {
       <div>
         <Header />
         <div class="pt-20 md:pt-24">
-          <div class="w-full md:w-11/12 mx-auto backgound-color pt-4 md:p-12 lg:p-12">
+          <div class="w-full md:w-11/12 mx-auto backgound-color pt-0 md:p-12 lg:p-12">
             <div class="content md:w-10/12 lg:w-6/12 2xl:w-5/12 mx-auto">
               <Show
                 when={resource.loading}
