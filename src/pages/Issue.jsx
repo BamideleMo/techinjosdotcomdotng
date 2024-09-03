eab4f9b871f9853e611324c0d1eb79efa7974199
bamidelemo <bamidelemo@gmail.com>
2024-09-03 03:24:33 +0100

    .

--- src/pages/Issue.jsx
+++ src/pages/Issue.jsx
@@ -281,7 +281,7 @@ function Issue() {
           <Show
             when={prevIssue()}
             fallback={
-              <span class="flex space-x-1 cursor-not-allowed text-gray-200">
+              <span class="flex space-x-1 cursor-not-allowed text-gray-600">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -328,7 +328,7 @@ function Issue() {
           <Show
             when={nextIssue()}
             fallback={
-              <span class="flex space-x-1 cursor-not-allowed text-gray-200">
+              <span class="flex space-x-1 cursor-not-allowed text-gray-600">
                 <span>NEXT</span>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
