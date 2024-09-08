5aa7130322b6711745432937440741e1e51890a6
bamidelemo <bamidelemo@gmail.com>
2024-09-08 08:09:44 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -76,9 +76,22 @@ function Header() {
         <div class="fixed w-full mx-auto z-50">
           <div class="w-60 mx-auto bg-yellow-100 border border-yellow-300 p-3 mt-6 space-x-3 rounded-md text-xs flex">
             <div class="w-7">
-              <img src={network} class="w-full" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                class="size-6 text-red-600"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75Zm0 8.25a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z"
+                  clip-rule="evenodd"
+                />
+              </svg>
             </div>
-            <div>Please check your internet connection.</div>
+            <div>
+              <b>Please check your internet connection.</b>
+            </div>
           </div>
         </div>
       </Show>
