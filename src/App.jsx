eff586b897eb909cdc25238300d63ce5a76014de
bamidelemo <bamidelemo@gmail.com>
2024-09-11 02:59:58 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
                 <div class="w-10/12 md:w-8/12 lg:w-10/12 mx-auto grid grid-cols-1 lg:grid-cols-2">
                   <div class="text-center lg:text-left">
                     <h1 class="uppercase">Weekly Newsletter</h1>
-                    <h1 class="text-2xl md:text-3xl lg:text-4xl xl:text-5xl font-normal drop-shadow-lg">
+                    <h1 class="text-3xl md:text-3xl lg:text-4xl xl:text-5xl font-normal drop-shadow-lg">
                       Focused on the Tech Ecosystem in Jos-Plateau state.
                     </h1>
                     <div class="my-6 text-lg">
@@ -71,11 +71,11 @@ function App() {
                       </A>
                     </div>
                   </div>
-                  <div class="overflow-hidden -mb-10">
+                  <div class="overflow-hidden -mb-10 h-96 bg-white">
                     <img
                       src={screen}
                       alt="tech in Jos mobile display"
-                      class="w-60 mx-auto md:w-96 lg:w-96 lg:float-right"
+                      class="lg:float-right"
                     />
                   </div>
                 </div>
