142a06fa20e68671e9af3a7eb8158f98de37a646
bamidelemo <bamidelemo@gmail.com>
2024-09-12 16:05:05 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -109,7 +109,7 @@ function Header() {
           </div>
         </div>
       </div>
-      <Show when={navigator.onLine}>
+      <Show when={!navigator.onLine}>
         <div class="fixed w-full mx-auto z-50">
           <div
             class="w-60 mx-auto bg-yellow-100 border border-yellow-300 p-3 
