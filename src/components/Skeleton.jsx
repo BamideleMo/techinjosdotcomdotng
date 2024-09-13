5aa5e05a1ec6a37e06e83e39d12f1e9153da56b7
bamidelemo <bamidelemo@gmail.com>
2024-09-13 16:27:41 +0100

    .

--- src/components/Skeleton.jsx
+++ src/components/Skeleton.jsx
@@ -4,18 +4,6 @@ function Skeleton() {
   return (
     <>
       <div
-        class="
-    skeleton 
-    skeleton-circle-multi-line
-  "
-        style="
-    --c-s: 80px;
-    --c-w: 100%;
-    --c-pos: top;
-    --lines: 3;
-  "
-      ></div>
-      <div
         class="skeleton skeleton-rect mx-auto"
         style="--rect-h: 150px; --c-w: 100%; --lines: 20;"
       ></div>
