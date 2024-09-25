b2e6291de2d0d85cf2730bc1bb6ed5e6140a791f
bamidelemo <bamidelemo@gmail.com>
2024-09-25 07:59:12 +0100

    .

--- src/App.jsx
+++ src/App.jsx
@@ -93,11 +93,11 @@ function App() {
                 </Show>
               </div>
             </div>
-            <div class="overflow-hidden border-b border-yellow-300 h-72 md:h-96 bg-white">
+            <div class="overflow-hidden h-72 md:h-96 bg-white">
               <img
                 src={screen}
                 alt="tech in Jos mobile display"
-                class="w-fit mx-auto lg:float-right"
+                class="w-11/12 mx-auto border-b border-yellow-300 lg:float-right"
               />
             </div>
           </div>
